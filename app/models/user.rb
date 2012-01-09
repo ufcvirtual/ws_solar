4d20561ef695cb3758e713a87289c6df7f9eda29
Wedson Lima <wedson.sousa.lima@gmail.com>
2012-01-09 15:17:32 -0300

    Autenticacao por token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :encryptable
+
+  devise :database_authenticatable, :registerable, :validatable, :encryptable,
+    :token_authenticatable # autenticacao por token
+
+  # before_save :ensure_authentication_token!
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :username, :email, :password, :password_confirmation, :remember_me
+  attr_accessible :username, :email, :password, :password_confirmation, :authentication_token
+  
+  # def ensure_authentication_token!
+    # reset_authentication_token! if authentication_token.blank? 
+  # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,51 +1,18 @@
 WsSolar::Application.routes.draw do
 
-  devise_for :users, :path_names => {:sign_up => 'register'}
+  devise_for :users, :controllers => { :sessions => "sessions" }
 
   devise_scope :user do
-    get "login", :to => "devise/sessions#new"
-    get "logout", :to => "devise/sessions#destroy"
+    resources :sessions, :only => [:create, :destroy]
   end
 
-  get "home/index"
+  resources :token_authentications, :only => [:create, :destroy]
 
   resources :discussions do
     resources :posts
   end
 
   root :to => "home#index"
-
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
 
   # Sample resource route with more complex sub-resources
   #   resources :products do
@@ -61,12 +28,6 @@ WsSolar::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
-
-  # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
