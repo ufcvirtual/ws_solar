bad826ee2eeae762c65743888eee2eed27623ef3
Wedson Lima <wedson.sousa.lima@gmail.com>
2012-01-26 14:45:50 -0300

    Criar um post atraves de REST

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -41,24 +41,32 @@ class PostsController < ApplicationController
     @discussion_post = DiscussionPost.find(params[:id])
   end
 
-  # POST /posts
-  # POST /posts.xml
+  # POST /discussions/:id/posts
+  # POST /discussions/:id/posts.xml
   def create
+
+    # parametros necessarios para se criar um post
+    params[:discussion_post][:user_id] = current_user.id
+    params[:discussion_post][:discussion_id] = params[:discussion_id]
+    # params[:discussion_post][:profile_id] = params[:profile_id]
+
     @discussion_post = DiscussionPost.new(params[:discussion_post])
 
     respond_to do |format|
       if @discussion_post.save
         format.html { redirect_to(discussion_post_path(Discussion.find(params[:discussion_id]), @discussion_post), :notice => 'Discussion post was successfully created.') }
         format.xml  { render :xml => @discussion_post, :status => :created, :location => @discussion_post }
+        format.json  { render :json => {:result => 1}, :status => :created }
       else
         format.html { render :action => "new" }
         format.xml  { render :xml => @discussion_post.errors, :status => :unprocessable_entity }
+        format.json  { render :json => {:result => 0}, :status => :unprocessable_entity }
       end
     end
   end
 
-  # PUT /posts/1
-  # PUT /posts/1.xml
+  # PUT /discussions/:id/posts/1
+  # PUT /discussions/:id/posts/1.xml
   def update
     @discussion_post = DiscussionPost.find(params[:id])
 
