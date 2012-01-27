87f0af116bacccb457eb9e6146ab1966face77ca
Wedson Lima <wedson.sousa.lima@gmail.com>
2012-01-27 17:22:12 -0300

    Retirando modificacao de token. Sera verificado uma melhor maneira de fazer isso.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < Devise::SessionsController
 
       format.json {
         warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#new")
-        current_user.reset_authentication_token!
+        # current_user.reset_authentication_token! # modifica o token do usuario ao realizar uma autenticacao
         render :status => 200, :json => { :session => { :error => "Success", :auth_token => current_user.authentication_token } }
       }
     end
