1d99daca6eddf6fbf4ea85f29fa58b39685a16a8
Wedson Lima <wedson.sousa.lima@gmail.com>
2012-02-15 17:35:19 -0300

    Formato de hora - 24h

--- app/models/discussion_post.rb
+++ app/models/discussion_post.rb
@@ -9,7 +9,7 @@ class DiscussionPost < ActiveRecord::Base
   def self.find_all_by_discussion_id(discussion_id)
     query = <<SQL
         SELECT t3.name AS user_name, t3.username AS user_username,
-               t1.discussion_id, t1.id, t1.content, t1.profile_id, to_char(t1.updated_at::timestamp, 'YYYYMMDDHHMISS') AS updated, t1.parent_id
+               t1.discussion_id, t1.id, t1.content, t1.profile_id, to_char(t1.updated_at::timestamp, 'YYYYMMDDHH24MISS') AS updated, t1.parent_id
           FROM discussion_posts AS t1
           JOIN discussions      AS t2 ON t2.id = t1.discussion_id
           JOIN users            AS t3 ON t3.id = t1.user_id
