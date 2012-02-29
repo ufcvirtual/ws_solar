82d70813a474176ea39e5d03db4cb0bde1447c26
Wedson Lima <wedson.sousa.lima@gmail.com>
2012-02-29 15:21:14 -0300

    Verificando se o usuario possui imagem para carregar

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -3,16 +3,19 @@ class ImagesController < ApplicationController
   # Recupera imagens de todos os usuarios passados por id
   ##
   def users
-  	# criar um zip e enviar
-  	# send_data -- gz
+    # criar um zip e enviar
+    # send_data -- gz
 
-	style = params.include?('style') ? params[:style] : :small
-  	users = User.find(params[:id].split(','), :select => "id, photo_file_name")
+    style = params.include?('style') ? params[:style] : :small
+    users = User.find(params[:id].split(','), :select => "id, photo_file_name")
 
-  	users.each do |u|
-  		path = u.photo.path(style)
-  		send_file(path, { :content_type => 'image', :filename => u.id }) unless path.nil?
-  	end
+    path = nil
+    users.each do |u|
+        path = u.photo.path(style)
+        send_file(path, { :content_type => 'image', :filename => u.id }) unless path.nil?
+    end
+
+    head(:bad_request) and return if path.nil?
   end
 
 end
